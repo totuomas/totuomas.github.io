4a9f3d6bb2d96d104dde0c5c80d87b4cc4febfec
totuomas <to.tuomas@outlook.com>
2026-04-14 10:17:21 +0300

    Added individual fade ins for mobile responsiveness

--- script.js
+++ script.js
@@ -183,3 +183,20 @@ backToTopBtn.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); // animate only once
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+fadeElements.forEach((el) => {
+  observer.observe(el);
+});
